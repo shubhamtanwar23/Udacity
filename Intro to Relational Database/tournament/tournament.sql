6c38600cde7d7e25fc1a229937ccf92efe679ddd
Shubham Tanwar <Shubham Tanwar>
2017-06-01 19:36:30 +0530

    Updated Tables

--- Intro to Relational Database/tournament/tournament.sql
+++ Intro to Relational Database/tournament/tournament.sql
@@ -12,15 +12,11 @@
 -- player TABLE which stores the information for registered players
 CREATE TABLE player (
 id 	serial	 primary key,
-name	varchar(20),
-wins	smallint	DEFAULT 0,
-matches	smallint	DEFAULT 0);
+name	text);
 
 
 -- matches TABLE which stores information about the matches(pairing) and winner of it
 CREATE TABLE matches(
-id1	smallint	references	player(id),
-name1	varchar(20),
-id2	smallint	references	player(id),
-name2	varchar(20),
-winner	varchar(20));
+id	serial	primary key,
+winner	integer	references	player(id),
+loser	integer	references	player(id));
